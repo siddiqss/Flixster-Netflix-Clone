e34b7fc810d1f312f691c3bad732544ddfa4cb91
Talha Siddique <tstalhasiddique@gmail.com>
2021-09-07 22:33:16 +0500

    index.js updated!

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/Frontend/build', 'index.html'));
 });
 
-app.listen(8800 || process.env.PORT, () => {
+app.listen(8800 || process.env.PORT, '0.0.0.0', () => {
   console.log("Backend server is running...");
 });
