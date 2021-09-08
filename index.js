b30da68284cc60229558782f0630a1236b43333b
Talha Siddique <tstalhasiddique@gmail.com>
2021-09-08 18:29:12 +0500

    index.js updated!

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ dotenv.config();
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: true
+    useUnifiedTopology: true
   })
   .then(() => console.log("DB connection successful!"))
   .catch((err) => console.log(err));
