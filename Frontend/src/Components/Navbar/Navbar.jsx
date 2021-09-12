8e9cca87ed119c8045f99dfad29d7388402036c5
Talha Siddique <tstalhasiddique@gmail.com>
2021-09-13 00:27:27 +0500

    API folder updated!

--- Frontend/src/Components/Navbar/Navbar.jsx
+++ Frontend/src/Components/Navbar/Navbar.jsx
@@ -8,8 +8,12 @@ import { logout } from "../../authContext/AuthActions";
 
 function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const {dispatch} = useContext(AuthContext);
+  const [isClicked, setIsClicked] = useState(false);
+  const { dispatch } = useContext(AuthContext);
 
+  const handleClick = () => {
+    setIsClicked(true);
+  };
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
@@ -21,14 +25,14 @@ function Navbar() {
       <div className='container'>
         <div className='left'>
           <img src={logo} alt='' />
-          <Link to="/" className="link">
+          <Link to='/' className='link'>
             <span>Homepage</span>
           </Link>
-          <Link to="series" className="link">
-          <span className="navbarmainlinks">Series</span>
+          <Link to='series' className='link'>
+            <span className='navbarmainlinks'>Series</span>
           </Link>
-          <Link to="movies" className="link">
-          <span className="navbarmainlinks">Movies</span>
+          <Link to='movies' className='link'>
+            <span className='navbarmainlinks'>Movies</span>
           </Link>
           <span>New and Popular</span>
           <span>My List</span>
@@ -42,10 +46,10 @@ function Navbar() {
             alt=''
           />
           <div className='profile'>
-            <ArrowDropDown className='icon' />
+            <ArrowDropDown className='icon' onClick={handleClick} />
             <div className='options'>
               <span>Settings</span>
-              <span onClick={()=> dispatch(logout())}>Logout</span>
+              <span onClick={() => dispatch(logout())}>Logout</span>
             </div>
           </div>
         </div>
--- Readme.md
+++ Readme.md
@@ -1,2 +1,29 @@
-Update MONGO_URL in .env file.
-Also change the SECRET_KEY for security purpose. 
+# Flixster-Netflix Clone
+
+## Frontend
+- React JS with Context API
+- Material-UI Icons inserted
+- SASS based styling and animation
+
+## Backend
+- REST API with Node and Express JS
+- MongoDB
+- Firebase Storage for storing movies
+
+## Usage
+- Clone the repo
+- cd to API directory and install all the dependencies using `yarn`
+- Create a .env file and store the MONGO_URL and SECRET_KEY. Make sure that you provide the correct Mongo URI and secret key can be anything (it's for security purpose)
+- Now run `yarn start` to start the API server
+- Open another terminal, cd into Frontend and install dependencies using `yarn`
+- Create a .env file and assign the url of the backend server to REACT_APP_API_URL. In case of local machine, `http://localhost:PORT/api/`.
+- Run `yarn start` to start the frontend server
+- 
+### Preview
+
+![Home Page](https://github.com/siddiqss/Flixster-Netflix-Clone/blob/master/Images/main.JPG)
+
+![Movies Page](https://github.com/siddiqss/Flixster-Netflix-Clone/blob/master/Images/Movies.JPG)
+
+![Series Page](https://github.com/siddiqss/Flixster-Netflix-Clone/blob/master/Images/Series.JPG)
+
